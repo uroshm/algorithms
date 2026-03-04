0420c0edcc47a6064e1d7ab5da961f4e59feedc3
Uros <uroshm@gmail.com>
2026-03-03 22:03:59 -0500

    add pre and post deque ie stack

--- src/main/java/com/graphs/app/algorithm/DFS.java
+++ src/main/java/com/graphs/app/algorithm/DFS.java
@@ -1,6 +1,8 @@
 package com.graphs.app.algorithm;
 
 import com.graphs.app.data.Graph;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.LinkedList;
 import lombok.RequiredArgsConstructor;
@@ -12,11 +14,13 @@ public class DFS {
 
   private final Graph graph;
   HashMap<String, Boolean> visited = new HashMap<>();
+  Deque<String> preVisit = new ArrayDeque<>();
+  Deque<String> postVisit = new ArrayDeque<>();
   HashMap<String, Integer> cc = new HashMap<>();
 
   public void dfs() {
     var cc_counter = 0;
-
+    var clock = 0;
     for (var vertexList : graph.adjacencyList) {
       var currentRoot = vertexList.get(0);
       visited.put(currentRoot, false);
@@ -28,13 +32,13 @@ public class DFS {
       var currentRoot = vertexList.get(0);
       if (!visited.get(currentRoot)) {
         cc_counter++;
-        explore(graph.getAdjanceyListForVertex(currentRoot), cc_counter);
+        explore(graph.getAdjanceyListForVertex(currentRoot), cc_counter, clock);
       }
     }
     System.out.println("cc: " + cc_counter);
   }
 
-  void explore(LinkedList<String> adjacencyList, Integer cc_counter) {
+  void explore(LinkedList<String> adjacencyList, Integer cc_counter, Integer clock) {
     var vertex = adjacencyList.get(0);
     visited.put(vertex, true);
     System.out.println("visited: " + vertex);
@@ -48,7 +52,7 @@ public class DFS {
       var adjacentVertex = adjacencyList.get(i);
       var visitedFlag = visited.get(adjacentVertex);
       if (visitedFlag == null || !visitedFlag) {
-        explore(graph.getAdjanceyListForVertex(adjacentVertex), cc_counter);
+        explore(graph.getAdjanceyListForVertex(adjacentVertex), cc_counter, clock);
       }
     }
   }
