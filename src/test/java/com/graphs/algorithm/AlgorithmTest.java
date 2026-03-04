e2d9522534964bfcc2dba5be73e0608038be4f5d
Uros <uroshm@gmail.com>
2026-03-03 22:05:12 -0500

    remove demo test and testutil

--- src/test/java/com/graphs/algorithm/AlgorithmTest.java
+++ src/test/java/com/graphs/algorithm/AlgorithmTest.java
@@ -1,37 +1,15 @@
 package com.graphs.algorithm;
 
 import com.graphs.app.algorithm.DFS;
-import com.graphs.app.data.Graph;
-import java.util.LinkedList;
 import org.junit.jupiter.api.Test;
 
 public class AlgorithmTest {
 
+  private TestUtil testUtil = new TestUtil();
+
   @Test
-  void demo() {
-    var dfs = new DFS(setupDemo());
+  void DPV_Problem_3_3() {
+    var dfs = new DFS(testUtil.setup_DPV_3_3());
     dfs.dfs();
-  }
-
-  Graph setupDemo() {
-    var graph = new Graph();
-
-    graph.adjacencyList = new LinkedList[4];
-    graph.adjacencyList[0] = new LinkedList<String>();
-    graph.adjacencyList[0].add("A");
-    graph.adjacencyList[0].add("B");
-    graph.adjacencyList[1] = new LinkedList<String>();
-    graph.adjacencyList[1].add("B");
-    graph.adjacencyList[1].add("A");
-    graph.adjacencyList[1].add("C");
-
-    graph.adjacencyList[2] = new LinkedList<String>();
-    graph.adjacencyList[2].add("C");
-    graph.adjacencyList[2].add("B");
-
-    graph.adjacencyList[3] = new LinkedList<String>();
-    graph.adjacencyList[3].add("D");
-
-    return graph;
   }
 }
--- src/test/java/com/graphs/algorithm/TestUtil.java
+++ src/test/java/com/graphs/algorithm/TestUtil.java
@@ -82,25 +82,4 @@ public class TestUtil {
     graph.adjacencyList[7].add("H");
     return graph;
   }
-
-  Graph setupDemo() {
-    var graph = new Graph();
-
-    graph.adjacencyList = new LinkedList[4];
-    graph.adjacencyList[0] = new LinkedList<String>();
-    graph.adjacencyList[0].add("A");
-    graph.adjacencyList[0].add("B");
-    graph.adjacencyList[1] = new LinkedList<String>();
-    graph.adjacencyList[1].add("B");
-    graph.adjacencyList[1].add("A");
-    graph.adjacencyList[1].add("C");
-
-    graph.adjacencyList[2] = new LinkedList<String>();
-    graph.adjacencyList[2].add("C");
-    graph.adjacencyList[2].add("B");
-
-    graph.adjacencyList[3] = new LinkedList<String>();
-    graph.adjacencyList[3].add("D");
-    return graph;
-  }
 }
